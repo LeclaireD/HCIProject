8c72711ec1331c8c591137857e52a8f7cb8c2769
Adam Hacault <Adam Hacault>
2016-11-18 13:50:27 -0600

    Here are the changes i made to selectMovie ive changed the css and the js file

--- js/movie.js
+++ js/movie.js
@@ -24,6 +24,98 @@ $('#seatselector').click(function(){
 window.location.href='seatselector.html';
 });
 
+/*Adam stuff*/
+
+$('#m1').click(function(){
+	document.getElementById("poster").src = "juice.jpg"
+	document.getElementById("movieName").innerHTML = "BeetleJuice"
+	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	
+	document.getElementById("m1t1").className = "timeButtonOn"
+	document.getElementById("m1t1").disabled = false;
+	document.getElementById("m1t2").className = "timeButtonOn"
+	document.getElementById("m1t2").disabled = false;
+	document.getElementById("m1t3").className = "timeButtonOn"
+	document.getElementById("m1t3").disabled = false;
+	/*Others off*/
+	document.getElementById("m2t1").className = "timeButton"
+	document.getElementById("m2t1").disabled = true;
+	document.getElementById("m2t2").className = "timeButton"
+	document.getElementById("m2t2").disabled = true;
+	document.getElementById("m2t3").className = "timeButton"
+	document.getElementById("m2t3").disabled = true;
+	
+	document.getElementById("m3t1").className = "timeButton"
+	document.getElementById("m3t1").disabled = true;
+	document.getElementById("m3t2").className = "timeButton"
+	document.getElementById("m3t2").disabled = true;
+	document.getElementById("m3t3").className = "timeButton"
+	document.getElementById("m3t3").disabled = true;
+	});
+
+$('#m2').click(function(){
+	document.getElementById("poster").src = "eleven.jpeg"
+	document.getElementById("movieName").innerHTML = "Ocean's Elven"
+	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	
+	document.getElementById("m2t1").className = "timeButtonOn"
+	document.getElementById("m2t1").disabled = false;
+	document.getElementById("m2t2").className = "timeButtonOn"
+	document.getElementById("m2t2").disabled = false;
+	document.getElementById("m2t3").className = "timeButtonOn"
+	document.getElementById("m2t3").disabled = false;
+	/*Others off*/
+	document.getElementById("m1t1").className = "timeButton"
+	document.getElementById("m1t1").disabled = true;
+	document.getElementById("m1t2").className = "timeButton"
+	document.getElementById("m1t2").disabled = true;
+	document.getElementById("m1t3").className = "timeButton"
+	document.getElementById("m1t3").disabled = true;
+	
+	document.getElementById("m3t1").className = "timeButton"
+	document.getElementById("m3t1").disabled = true;
+	document.getElementById("m3t2").className = "timeButton"
+	document.getElementById("m3t2").disabled = true;
+	document.getElementById("m3t3").className = "timeButton"
+	document.getElementById("m3t3").disabled = true;
+	});
+
+$('#m3').click(function(){
+	document.getElementById("poster").src = "skyfall.jpg"; 
+	document.getElementById("movieName").innerHTML = "Skyfall"
+	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	
+	document.getElementById("m3t1").className = "timeButtonOn"
+	document.getElementById("m3t1").disabled = false;
+	document.getElementById("m3t2").className = "timeButtonOn"
+	document.getElementById("m3t2").disabled = false;
+	document.getElementById("m3t3").className = "timeButtonOn"
+	document.getElementById("m3t3").disabled = false;
+	/*Others off*/
+	document.getElementById("m1t1").className = "timeButton"
+	document.getElementById("m1t1").disabled = true;
+	document.getElementById("m1t2").className = "timeButton"
+	document.getElementById("m1t2").disabled = true;
+	document.getElementById("m1t3").className = "timeButton"
+	document.getElementById("m1t3").disabled = true;
+	
+	document.getElementById("m2t1").className = "timeButton"
+	document.getElementById("m2t1").disabled = true;
+	document.getElementById("m2t2").className = "timeButton"
+	document.getElementById("m2t2").disabled = true;
+	document.getElementById("m2t3").className = "timeButton"
+	document.getElementById("m2t3").disabled = true;
+	});
+
+$('#m1t1').click(function(){document.getElementById("movieTime").innerHTML = "3:20pm"});
+$('#m1t2').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
+$('#m1t3').click(function(){document.getElementById("movieTime").innerHTML = "8:15pm"});
+$('#m2t1').click(function(){document.getElementById("movieTime").innerHTML = "11:00am"});
+$('#m2t2').click(function(){document.getElementById("movieTime").innerHTML = "1:15pm"});
+$('#m2t3').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
+$('#m3t1').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
+$('#m3t2').click(function(){document.getElementById("movieTime").innerHTML = "8:30pm"});
+$('#m3t3').click(function(){document.getElementById("movieTime").innerHTML = "9:00pm"});
 
 });
 
