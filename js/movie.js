8f9b359befe7e01c85070cd97bdb7ce366781622
Adam Hacault <Adam Hacault>
2016-11-18 22:07:22 -0600

    Here are some changes, hopfully i didnt break anything

--- js/movie.js
+++ js/movie.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-
  $("#header").load("header.html"); 
 
 
@@ -9,7 +7,6 @@ $('#selectmovie').click(function(){
 location.href = "newpage.html"
 window.location.href='movieselector.html';
 });
-
 $('#selecttime').click(function(){
 window.location.href='timeselector.html';
 });
@@ -21,20 +18,18 @@ window.location.href='letuschoose.html';
 $('#paymentpage').click(function(){
 window.location.href='paymentpage.html';
 });
-
 $('#seatselector').click(function(){
 window.location.href='seatselector.html';
 });
 
-$('#paynow').click(function(){
-window.location.href='endslate.html';
-});
 /*Adam stuff*/
 
 $('#m1').click(function(){
 	document.getElementById("poster").src = "../images/juice.jpg"
 	document.getElementById("movieName").innerHTML = "Beetle Juice"
 	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	document.getElementById("seatselector").disabled = true;
+	document.getElementById("seatselector").className = "stylebuttonsOff";
 	
 	document.getElementById("m1t1").className = "timeButtonOn"
 	document.getElementById("m1t1").disabled = false;
@@ -62,6 +57,8 @@ $('#m2').click(function(){
 	document.getElementById("poster").src = "../images/eleven.jpeg"
 	document.getElementById("movieName").innerHTML = "Ocean's Elven"
 	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	document.getElementById("seatselector").disabled = true;
+	document.getElementById("seatselector").className = "stylebuttonsOff"
 	
 	document.getElementById("m2t1").className = "timeButtonOn"
 	document.getElementById("m2t1").disabled = false;
@@ -89,6 +86,8 @@ $('#m3').click(function(){
 	document.getElementById("poster").src = "../images/skyfall.jpg"; 
 	document.getElementById("movieName").innerHTML = "Skyfall"
 	document.getElementById("movieTime").innerHTML = "Not Chosen"
+	document.getElementById("seatselector").disabled = true;
+	document.getElementById("seatselector").className = "stylebuttonsOff"
 	
 	document.getElementById("m3t1").className = "timeButtonOn"
 	document.getElementById("m3t1").disabled = false;
@@ -112,15 +111,33 @@ $('#m3').click(function(){
 	document.getElementById("m2t3").disabled = true;
 	});
 
-$('#m1t1').click(function(){document.getElementById("movieTime").innerHTML = "3:20pm"});
-$('#m1t2').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
-$('#m1t3').click(function(){document.getElementById("movieTime").innerHTML = "8:15pm"});
-$('#m2t1').click(function(){document.getElementById("movieTime").innerHTML = "11:00am"});
-$('#m2t2').click(function(){document.getElementById("movieTime").innerHTML = "1:15pm"});
-$('#m2t3').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
-$('#m3t1').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"});
-$('#m3t2').click(function(){document.getElementById("movieTime").innerHTML = "8:30pm"});
-$('#m3t3').click(function(){document.getElementById("movieTime").innerHTML = "9:00pm"});
+$('#m1t1').click(function(){document.getElementById("movieTime").innerHTML = "3:20pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m1t2').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m1t3').click(function(){document.getElementById("movieTime").innerHTML = "8:15pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m2t1').click(function(){document.getElementById("movieTime").innerHTML = "11:00am"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m2t2').click(function(){document.getElementById("movieTime").innerHTML = "1:15pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m2t3').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m3t1').click(function(){document.getElementById("movieTime").innerHTML = "4:20pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m3t2').click(function(){document.getElementById("movieTime").innerHTML = "8:30pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
+$('#m3t3').click(function(){document.getElementById("movieTime").innerHTML = "9:00pm"
+							document.getElementById("seatselector").disabled = false;
+							document.getElementById("seatselector").className = "stylebuttons";});
 
 	/*idk what i am doing 
 $("#one").click(function(){
@@ -142,7 +159,8 @@ $("#seven").click(function(){
 
 $(“#nine”).click(function(){
     $("p").hide();
-	*/	
 		
+		
+});*/
 });
 
